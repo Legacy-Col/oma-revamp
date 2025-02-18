08dcab4411de5a0c932353b549601cc20b379103
Legacy-Col <lynxlestley@gmail.com>
2025-02-18 12:19:29 +0100

    me

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import { Box, Button } from '@mui/material';
 import React from 'react'
+import CourtImg from '../assets/Courthouse.jpg'
 
 
 const Hero = () => {
   return (
     <div
       className="h-screen relative bg-cover bg-center "
-      style={{ backgroundImage: "url('src/assets/Courthouse.jpg')" }}
-    >
+      style={{ backgroundImage: `url (${CourtImg})` }}    >
       <section className="absolute inset-0 bg-black bg-opacity-65 flex items-center justify-center">
         <div className="text-center text-white p-4">
           <h1 className="text-4xl md:text-6xl font-bold my-24">
